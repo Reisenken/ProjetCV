3f51ac862f0ba66e5d729b98461f77de5685759a
Benoit <carpentier.be@gmail.com>
2018-10-09 16:52:32 +0200

    Projet du CV : les boutons sont faits

---
+++ ProjetCV.js
@@ -0,0 +1,13 @@
+
+function animation(c) {
+if (c = 1) {
+
+
+}
+
+
+
+    document.getElementById("onClick").style.display = "None";
+    document.getElementById("onClick").style.display = "Block";
+
+}
